aff20e6f85e3c9e19efcf7df4ad5f286f7772a99
huunghiadvc <huunghia98er@gmail.com>
2024-02-25 09:16:53 +0700

    update products

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -67,31 +67,39 @@
                 <table class="table">
                     <tr>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="name"  placeholder="Name..." />
+                            <input form="createProductForm" id="nameInput" type="text" class="form-control" oninput="nameValid()" name="name"  placeholder="Name..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="price"  placeholder="Price..." />
+                            <input form="createProductForm" id="priceInput" class="form-control" name="price" oninput="priceValid()" placeholder="Price..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="height"  placeholder="Height..." />
+                            <input form="createProductForm" id="heightInput" oninput="heightValid()" class="form-control" name="height" placeholder="Height..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="length_col"  placeholder="Length..." />
+                            <input form="createProductForm" id="lengthInput" oninput="lengthValid()"  class="form-control" name="length_col" placeholder="Length..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="width"  placeholder="Width..." />
+                            <input form="createProductForm" id="widthInput" oninput="widthValid()" class="form-control" name="width" placeholder="Width..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="base_unit" placeholder="Unit..." />
+                            <input form="createProductForm" id="baseUnitInput" oninput="baseUnitValid()" class="form-control" name="base_unit" placeholder="Unit..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="producer"  placeholder="Producer..." />
+                            <input form="createProductForm" id="producerInput" oninput="producerValid()" class="form-control" name="producer" placeholder="Producer..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control" name="quantity"  placeholder="Quantity..." />
+                            <input form="createProductForm" id="quantityInput" oninput="quantityValid()" class="form-control" name="quantity" placeholder="Quantity..." />
+                            <div class="error-message"></div>
                         </td>
                         <td class="align-middle col-1">
-                            <input form="createProductForm" class="form-control bg-dark text-white" type="submit" value="Create"/>
+                            <input form="createProductForm" id="btn-create" class="form-control" type="submit" value="Create" disabled/>
                         </td>
                     </tr>
                 </table>
@@ -176,6 +184,117 @@
     <div class="pagination d-flex justify-content-center">
         {{ $products->onEachSide(2)->links() }}
     </div>
+
+    <script>
+
+        let valueValid = 0;
+
+        const errorMessage = (message, inputElement) => {
+            inputElement.classList.add('btn-outline-danger');
+
+            const errorElement = inputElement.nextElementSibling;
+            errorElement.classList.add('text-danger');
+            errorElement.textContent = message;
+        }
+
+        const successMessage = (inputElement) => {
+            valueValid++;
+            inputElement.classList.remove('btn-outline-danger');
+            inputElement.classList.add('btn-outline-success');
+
+            const errorElement = inputElement.nextElementSibling;
+            errorElement.textContent = '';
+
+            if (valueValid === 8) {
+                const btnCreate = document.getElementById('btn-create');
+                btnCreate.disabled = false;
+                btnCreate.style.backgroundColor = '#218838';
+                btnCreate.style.color = '#ffffff';
+            }
+        }
+
+        const numberValid = (name, inputElement) => {
+
+            if (inputElement.value.trim() === '') {
+                errorMessage(name + ' can\'t be empty!', inputElement);
+            }
+            else if (isNaN(parseFloat(inputElement.value))) {
+                errorMessage(name + ' can\'t be string!', inputElement);
+            }
+            else if (inputElement.value < 1) {
+                errorMessage(name + ' can\'t < 1!', inputElement);
+            } else {
+                successMessage(inputElement);
+            }
+        }
+
+        const stringValid = (name, inputElement) => {
+
+            if (!isNaN(parseFloat(inputElement.value))) {
+                errorMessage(name + ' can\'t be number!', inputElement);
+            } else if (inputElement.value.trim() === '') {
+                errorMessage(name + ' can\'t be empty!', inputElement);
+            } else {
+                successMessage(inputElement);
+            }
+        }
+
+        const nameValid = () => {
+            const name = 'Name';
+            const inputElement = document.getElementById("nameInput");
+            stringValid(name, inputElement);
+        }
+
+        const producerValid = () => {
+            const name = 'Producer';
+            const inputElement = document.getElementById("producerInput");
+            stringValid(name, inputElement);
+        }
+
+        const priceValid = () => {
+            const name = 'Price';
+            const inputElement = document.getElementById("priceInput");
+            numberValid(name, inputElement);
+        }
+
+        const heightValid = () => {
+            const name = 'Height';
+            const inputElement = document.getElementById("heightInput");
+            numberValid(name, inputElement);
+        }
+
+        const lengthValid = () => {
+            const name = 'Length';
+            const inputElement = document.getElementById("lengthInput");
+            numberValid(name, inputElement);
+        }
+
+        const widthValid = () => {
+            const name = 'Width';
+            const inputElement = document.getElementById("widthInput");
+            numberValid(name, inputElement);
+        }
+
+        const quantityValid = () => {
+            const name = 'Quantity';
+            const inputElement = document.getElementById("quantityInput");
+            numberValid(name, inputElement);
+        }
+
+        const baseUnitValid = () => {
+            const inputElement = document.getElementById("baseUnitInput");
+
+            if (!isNaN(parseFloat(inputElement.value))) {
+                errorMessage('Base unit can\'t be number!', inputElement);
+            } else if (inputElement.value.trim() === '') {
+                errorMessage('Base unit can\'t be empty!', inputElement);
+            } else if (inputElement.value.length > 3) {
+                errorMessage('Base unit length can\'t be greater than 3!', inputElement);
+            }else {
+                successMessage(inputElement);
+            }
+        }
+    </script>
 
     <script>
         $(document).ready(function () {
